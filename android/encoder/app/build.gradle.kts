6d80953f885ec98b016d0f4f693553ec17316177
SergeyBEST <sergej_morozov_1999@mail.ru>
2026-08-14 18:10:52 +0500

    Update build.gradle.kts

--- android/encoder/app/build.gradle.kts
+++ android/encoder/app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.encoder"
+        applicationId = "ru.sergey.encoder"
         minSdk = 24
         targetSdk = 35
         versionCode = 4
