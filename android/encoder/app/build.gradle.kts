21b793bace0689b30713bfdf8d70eb068212fef9
SergeyBEST <sergej_morozov_1999@mail.ru>
2026-08-14 16:28:47 +0500

    Убираю несовместимый с AGP 9 блок kotlin

--- android/encoder/app/build.gradle.kts
+++ android/encoder/app/build.gradle.kts
@@ -28,10 +28,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlin {
-        jvmToolchain(17)
-    }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
